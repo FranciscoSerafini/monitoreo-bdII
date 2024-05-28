928d957f7ee5db6d8275ec65b06ca43eb9bf0618
Francisco Serafini Giorgi <franciscoserafinigiorgi@gmail.com>
2024-05-28 11:28:19 -0300

    actualizacion del codigo

--- main.py
+++ main.py
@@ -3,18 +3,18 @@ import random
 import time
 from faker import Faker
 
-#conexion base de datos
+# conexión base de datos
 server = "DESKTOP-9345VIR"
 database = "Northwind"
-username = ""
-password = ""
+username = "Py"
+password = "123"
 
 connection_string = (
     f'DRIVER={{ODBC Driver 17 for SQL Server}};'
     f'SERVER={server};'
     f'DATABASE={database};'
     f'UID={username};'
-    f'PWD={password}'
+    f'PWD={password};'
 )
 
 # Intentar conectar a la base de datos
@@ -30,61 +30,51 @@ try:
 except pyodbc.Error as ex:
     print("Error al conectar a la base de datos:")
     print(ex)
-finally:
-    # Cerrar la conexión si se abrió
-    if 'conexion' in locals() and conexion:
-        comandos.close()
-        conexion.close()
 
-conexion = pyodbc.connect(connection_string)
-comandos = conexion.comandos()
-
-#generador de datos
-fake = Faker()
-
-#creacion de una nueva tabla
+# Crear la tabla TestTable si no existe
 comandos.execute('''
-    CREATE TABLE TestTable (
-        Id INT PRIMARY KEY,
-        Name NVARCHAR(100),
-        Age INT,
-        Salary DECIMAL(18,2),
-        CreateDate DATETIME
-    )
-'''  
-)
-
+    IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'TestTable')
+    BEGIN
+        CREATE TABLE TestTable (
+            Id INT IDENTITY(1,1) PRIMARY KEY,
+            Name NVARCHAR(100),
+            Age INT,
+            Salary DECIMAL(18,2),
+            CreateDate DATETIME
+        )
+    END
+''')
 conexion.commit()
 
-#insercion de datos
-tiempo = time.time()
-while time.time() - tiempo < 10:
-    for i in range(1000): #inserta lotes de 1000 registros
-        id = random.randint(1, 1000000)
+# Generador de datos
+fake = Faker()
+
+# Inserción de datos
+tiempo_inicio = time.time()
+while time.time() - tiempo_inicio < 10:
+    for i in range(1000):  # Insertar lotes de 1000 registros
         name = fake.name()
         age = random.randint(18, 78)
         salary = round(random.uniform(30000, 150000), 2)
         create_date = fake.date_time_this_year()
-        
-        comandos.execute('''
-            INSERT INTO TestTable (Id, Name, Age, Salary, CreateDate)
-            VALUES (?, ?, ?, ?, ?)
-        ''', (id, name, age, salary, create_date))
-    conexion.commit()
-    
-#actualizar el 30 % de los registros
 
-total_registros = comandos.execute('SELECT COUNT(*) FROM TestTable').fetchaval()
+        comandos.execute('''
+            INSERT INTO TestTable (Name, Age, Salary, CreateDate)
+            VALUES (?, ?, ?, ?)
+        ''', (name, age, salary, create_date))
+        conexion.commit()  # Cometer cada inserción aquí
+
+# Actualizar el 30 % de los registros
+total_registros = comandos.execute('SELECT COUNT(*) FROM TestTable').fetchone()[0]
 registros_update = int(total_registros * 0.3)
 comandos.execute('''
     UPDATE TestTable
     SET Salary = Salary * 1.1
     WHERE Id IN (SELECT TOP (?) Id FROM TestTable ORDER BY NEWID())
-''', (registros_update,))  
+''', (registros_update,))
 conexion.commit()
 
-#eliminar registros
-
+# Eliminar registros
 registros_delete = int(total_registros * 0.5)
 comandos.execute('''
     DELETE FROM TestTable
@@ -92,9 +82,6 @@ comandos.execute('''
 ''', (registros_delete,))
 conexion.commit()
 
-#eliminacion de tabla
-comandos.execute('DROP TABLE TestTable')
-conexion.commit()
-
+# Cerrar el cursor y la conexión
 comandos.close()
 conexion.close()
